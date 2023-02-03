48ee2322f47512eb83c76347701a33fceb4d69dc
Nabeeha Ali <alinabeeha@gmail.com>
2023-02-03 15:22:47 -0500

    sled ui done

--- Assets/Scripts/Nabeeha/SleddingGame/SledSceneSetup.cs
+++ Assets/Scripts/Nabeeha/SleddingGame/SledSceneSetup.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using System.Linq;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SledSceneSetup : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class SledSceneSetup : MonoBehaviour
 
     float timePassed;
     bool gameDone = false;
+
+    public GameObject p1State, p2State, p3State, p4State;
 
     public static List<MinigamePoints> sledpoints = new List<MinigamePoints>();
     public static List<MinigamePoints> sleddistinct;
@@ -37,15 +40,46 @@ public class SledSceneSetup : MonoBehaviour
         //checking if there is one player standing on ice
         if(SledGame.ranking == 1)
         {
+            //TODO: add delay
             EndGame();
             countdown.SetText("Game Over!");
             gameDone = true;
+        }
+
+        //UI updates
+        for (int i = 0; i < GameObject.FindGameObjectsWithTag("Player").Length; i++)
+        {
+            if (GameObject.FindGameObjectsWithTag("Player")[i].transform.GetChild(0).gameObject.GetComponent<SledGame>().inWater == true)
+            {
+                string tag = GameObject.FindGameObjectsWithTag("Player")[i].transform.GetChild(0).gameObject.tag;
+                if(tag == "Player 1")
+                {
+                    p1State.GetComponent<Image>().CrossFadeAlpha(0.5f, 1.0f, true);
+                    p1State.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().SetText("" + sledpoints.Find(x => x.playerID.Contains("StevetheCapy")).playerPoints);
+                }
+                else if (tag == "Player 2")
+                {
+                    p2State.GetComponent<Image>().CrossFadeAlpha(0.5f, 1.0f, true);
+                    p2State.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().SetText("" + sledpoints.Find(x => x.playerID.Contains("HippotheFlower")).playerPoints);
+                }
+                else if (tag == "Player 3")
+                {
+                    p3State.GetComponent<Image>().CrossFadeAlpha(0.5f, 1.0f, true);
+                    p3State.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().SetText("" + sledpoints.Find(x => x.playerID.Contains("ScooberttheNerd")).playerPoints);
+                }
+                else if (tag == "Player 4")
+                {
+                    p4State.GetComponent<Image>().CrossFadeAlpha(0.5f, 1.0f, true);
+                    p4State.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().SetText("" + sledpoints.Find(x => x.playerID.Contains("OctaviustheGangster")).playerPoints);
+                }
+
+            }
         }
     }
 
     void EndGame()
     {
-        Debug.Log("The game is over now");
+        //Debug.Log("The game is over now");
         for (int i = 0; i < GameObject.FindGameObjectsWithTag("Player").Length; i++)
         {
             if(GameObject.FindGameObjectsWithTag("Player")[i].transform.GetChild(0).gameObject.GetComponent<SledGame>().inWater == false)
