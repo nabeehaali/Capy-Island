4e8b5b5144b3d0aa659d08f78790ceb8e58ac278
redchamelon9 <48802350+redchamelon9@users.noreply.github.com>
2023-03-10 20:14:28 -0500

    Renamed scripts so we don't get errors
    - Added 2 at the end of all the hide smash script class names

--- Assets/Scripts/Andrew/HideSmash/StatueRotation.cs
+++ Assets/Scripts/Andrew/HideSmash/StatueRotation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatueRotation : MonoBehaviour
+public class StatueRotation2 : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
